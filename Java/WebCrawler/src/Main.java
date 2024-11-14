385671ccc8855460aaac7d66d86eb955a9cafa39
Daniel Musto <danielmusto06@gmail.com>
2024-11-14 18:21:59 +0100

    aggiunto comando shutdown per chiudere la threadPool tramite controllo che tutti i task avessero terminato l'esecuzione. aggiunta stampa dei link visitati/raccolti e aggiunta di una variabile che contiene i link non validi

--- Java/WebCrawler/src/Main.java
+++ Java/WebCrawler/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        WebCrawler w = new WebCrawler(1); //partendo da 0
-        w.crawl("https://www.wikipedia.com");
+        WebCrawler w = new WebCrawler(2, 15); //livello parte da 0; minimo thread: 1
+        float startTime, endTime;
+
+        System.out.println("\nInizio crawling\n");
+        startTime = System.nanoTime();
+
+        w.crawl("https://www.itisrossi.edu.it/");
+
+        endTime = System.nanoTime();
+        System.out.println("\nFine crawling\n");
+        System.out.println("Tempo impiegato: " + (endTime - startTime) / 1000000000 + " secondi");
+        System.out.println("Link visitati: " + w.visited.size());
+        System.out.println("Link raccolti: ");
+        for (int i = 0; i < w.gathered.size(); i++)
+            System.out.println("Livello " + i + ": " + w.gathered.get(i).size());
+        System.out.println("Link non validi: " + w.invalidLinks.size());
     }
 }
--- Java/WebCrawler/src/WebCrawler.java
+++ Java/WebCrawler/src/WebCrawler.java
@@ -1,10 +1,8 @@
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -14,49 +12,63 @@ public class WebCrawler {
 
     private final int maxLevel;
 
-    Set<String> visited = Collections.synchronizedSet(new HashSet<>());
-    ConcurrentHashMap<Integer, ArrayList<String>> gathered = new ConcurrentHashMap<>();
+    public Set<String> visited = Collections.synchronizedSet(new HashSet<>());
+    public ConcurrentHashMap<Integer, ArrayList<String>> gathered = new ConcurrentHashMap<>();
+    public Set<String> invalidLinks = Collections.synchronizedSet(new HashSet<>());
 
-    ExecutorService threadPool = Executors.newFixedThreadPool(10);
+    ThreadPoolExecutor threadPool;
 
-    public WebCrawler(int maxLevel) {
+    public WebCrawler(int maxLevel, int numThread) {
         this.maxLevel = Math.max(maxLevel, 0);
+        threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(Math.max(numThread, 1));
         for(int i=0; i<=maxLevel; i++)
             gathered.put(i, new ArrayList<>());
     }
 
     public void crawl(String url){
         crawlWrapper(url, 0);
+
+        while(threadPool.getActiveCount() > 0){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        threadPool.shutdown();
     }
 
     public void crawlWrapper(String url, int level){
 
-        synchronized(visited){
-            visited.add(url);
-        }
         gathered.get(level).add(url);
 
         try{
             Connection con = Jsoup.connect(url);
             Document doc = con.get();
 
+            synchronized(visited){
+                visited.add(url);
+            }
+
             for(Element link : doc.select("a[href]")){
 
-                String title = link.attr("title");
                 String hrefURL = link.absUrl("href");
 
                 if(!gathered.get(level).contains(hrefURL)){
                     gathered.get(level).add(hrefURL);
-                    System.out.println(title + ": " + hrefURL);
+                    System.out.println("livello " + level + " ->" + hrefURL);
                 }
 
                 if(level < maxLevel && !visited.contains(hrefURL))
                     threadPool.execute(() -> crawlWrapper(link.absUrl("href"), level+1));
             }
 
-        }catch(IOException e){
+        }catch(IOException | IllegalArgumentException e){
             System.out.println(e.getMessage());
-            throw new Error(e);
+            synchronized (invalidLinks){
+                invalidLinks.add(url);
+            }
         }
     }
 }
